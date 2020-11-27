6efa8bc1e72c645a3fb29b343696167e66b9b29f
Jorge Martinez <martinezjorge273@gmail.com>
2020-11-27 14:55:11 -0600

    Added Ikov and DreamScape RSPS files but buggy in linux

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+var robot = require("robotjs");
+
+
+function main() {
+    console.log("Starting...");
+    sleep(4000);
+
+    robot.moveMouse(0, 0);
+    robot.mouseClick();
+    console.log("Done.");
+}
+
+function sleep(ms) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+}
+
+main();
