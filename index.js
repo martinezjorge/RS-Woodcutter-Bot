7545f5417075f1eaf0c7977c3edc2f714b8b1698
Jorge <martinezjorge273@gmail.com>
2020-11-27 15:05:05 -0800

    Loops between two trees and drops the logs each time

--- index.js
+++ index.js
@@ -7,14 +7,46 @@ function main() {
 
     var number_of_loops = 0;
 
+    // first tree
+    var first_tree_x = 751;
+    var first_tree_y = 511;
+
+    // second tree
+    var second_tree_x = 1137;
+    var second_tree_y = 490;
+
     while (number_of_loops < 5) {
-        robot.moveMouseSmooth(830, 454);
+        // chop down first tree
+        robot.moveMouseSmooth(first_tree_x, first_tree_y);
         robot.mouseClick();
         sleep(8000);
+
+        dropLogs();
+
+        // chop down second tree
+        robot.moveMouseSmooth(second_tree_x, second_tree_y);
+        robot.mouseClick();
+        sleep(8000);
+
+        dropLogs();
+
         number_of_loops++;
     }
     
     console.log("Done.");
+}
+
+function dropLogs() {
+    // Set coordinates of inventory item you want to drop
+    var inventory_x = 1842;
+    var inventory_y = 797;
+
+    // drop logs from the inventory
+    robot.moveMouseSmooth(inventory_x, inventory_y);
+    robot.mouseClick('right');
+    robot.moveMouseSmooth(inventory_x, inventory_y + 70);
+    robot.mouseClick();
+    sleep(1000);    
 }
 
 function sleep(ms) {
