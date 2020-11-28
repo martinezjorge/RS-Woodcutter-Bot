d1814bfce64d22e91d746e85caec3e7592f8a232
Jorge <martinezjorge273@gmail.com>
2020-11-27 16:04:23 -0800

    Finds trees by trunk pixel color, rotates camera if no trees found

--- index.js
+++ index.js
@@ -7,24 +7,18 @@ function main() {
 
     var number_of_loops = 0;
 
-    // first tree
-    var first_tree_x = 751;
-    var first_tree_y = 511;
-
-    // second tree
-    var second_tree_x = 1137;
-    var second_tree_y = 490;
-
     while (number_of_loops < 5) {
-        // chop down first tree
-        robot.moveMouseSmooth(first_tree_x, first_tree_y);
-        robot.mouseClick();
-        sleep(8000);
 
-        dropLogs();
+        var tree = findTree();
+        // if we can't find a tree, write an error message and exit the loop
+        if (tree === false) {
+            rotateCamera();
+            console.log("Could not find a tree")
+            continue;
+        }
 
-        // chop down second tree
-        robot.moveMouseSmooth(second_tree_x, second_tree_y);
+        // chop down the tree we found
+        robot.moveMouseSmooth(tree.x, tree.y);
         robot.mouseClick();
         sleep(8000);
 
@@ -49,8 +43,59 @@ function dropLogs() {
     sleep(1000);    
 }
 
+function testScreenCapture() {
+    // taking a screenshot
+    var img = robot.screen.capture(0, 0, 1920, 1080);
+
+    var pixel_color = img.colorAt(30, 18);
+
+    console.log(pixel_color);
+}
+
+function findTree() {
+    var x = 300, y = 300, width = 1300, height = 400;
+    var img = robot.screen.capture(x, y, width, height);
+
+    var tree_colors = ["705634", "705d39", "765b37", "7d6039", "7a5d39"];
+
+    for (var i = 0; i < 1000; i++) {
+        var random_x = getRandomInt(0, width-1);
+        var random_y = getRandomInt(0, height-1);
+        var sample_color = img.colorAt(random_x, random_y);
+
+        if (tree_colors.includes(sample_color)) {
+            var screen_x = random_x + x;
+            var screen_y = random_y + y;
+
+            console.log("Found a tree at: " + screen_x + "," + screen_y + " color " + sample_color);
+            return {
+                x: screen_x,
+                y: screen_y
+            }
+        }
+    }
+
+    // Did not find the color in the screenshot
+    return false;
+}
+
+function rotateCamera() {
+    console.log("Rotating Camera");
+    robot.keyToggle('right', 'down');
+    sleep(1000);
+    robot.keyToggle('right', 'up');
+}
+
 function sleep(ms) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// testScreenCapture();
 
 main();
